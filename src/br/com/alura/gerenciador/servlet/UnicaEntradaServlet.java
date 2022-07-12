a7ba4392d72cb3d953d54a773751e5c26b31f6d2
Gabriel <g.dessilva95@gmail.com>
2022-07-11 22:04:58 -0300

    Criando o controlador

--- src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -8,23 +8,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.gerenciador.acao.AlteraEmpresa;
 import br.com.alura.gerenciador.acao.ListaEmpresas;
+import br.com.alura.gerenciador.acao.MostraEmpresa;
+import br.com.alura.gerenciador.acao.NovaEmpresa;
+import br.com.alura.gerenciador.acao.RemoveEmpresa;
 
 @WebServlet("/entrada")
 public class UnicaEntradaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String paramAcao = request.getParameter("acao");
 		
-		if(paramAcao.equals("ListaEmpresas")) {
+		if (paramAcao.equals("ListaEmpresas")) {
 			ListaEmpresas list = new ListaEmpresas();
 			list.executa(request, response);
-				
-		}else if(paramAcao.equals("RemoveEmpresas")) {
-			System.out.println("RemoveEmpresas");
-		}else if(paramAcao.equals("MostraEmpresas")) {
-			System.out.println("MostraEmpresas");
+
+		} else if (paramAcao.equals("RemoveEmpresa")) {
+			RemoveEmpresa remove = new RemoveEmpresa();
+			remove.executa(request, response);
+
+		} else if (paramAcao.equals("MostraEmpresa")) {
+			MostraEmpresa mostrar = new MostraEmpresa();
+			mostrar.executa(request, response);
+			
+		} else if (paramAcao.equals("AlteraEmpresa")) {
+			AlteraEmpresa emp = new AlteraEmpresa();
+			emp.altera(request, response);
+			
+		}else if (paramAcao.equals("NovaEmpresa")) {
+			NovaEmpresa empresa = new NovaEmpresa();
+			empresa.nova(request, response);
 		}
 	}
 
